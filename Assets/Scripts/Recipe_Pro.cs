09380e0b9efff18b4b05fb1cee070c31c1748bd6
wlsdkrla <wlswlsdk23@naver.com>
2024-03-08 04:11:14 +0900

    Scene / Script (레시피, 상점 연동)

--- Assets/Scripts/Recipe_Pro.cs
+++ Assets/Scripts/Recipe_Pro.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -14,31 +15,59 @@ public class Recipe_Pro : MonoBehaviour
     public GameObject soysourceUI;
 
     Store_Pro2 store;
-    Button button;
-
+    
     void Start()
     {
-        store = FindObjectOfType<Store_Pro2>();
+        store = FindObjectOfType<Store_Pro2>(); // 비활성화 오브젝트는 찾지 못함
     }
-    public void OnCLick()
+    public void OnClickButton_1()
     {
-        
+        store.saveRecipe[0].SetActive(true); // UI 리스트 0 번째 접근 후 UI 활성화
+        SetfalseUI(0); // 0을 제외한 나머지 UI 비활성화
+    }
+    public void OnClickButton_2()
+    {
+        store.saveRecipe[1].SetActive(true);
+        SetfalseUI(1);
 
     }
-   
+    public void OnClickButton_3()
+    {
+        store.saveRecipe[2].SetActive(true);
+        SetfalseUI(2);
+    }
+    public void OnClickButton_4()
+    {
+        store.saveRecipe[3].SetActive(true);
+        SetfalseUI(3);
+    }
+    public void OnClickButton_5()
+    {
+        store.saveRecipe[4].SetActive(true);
+        SetfalseUI(4);
+    }
+    public void OnClickButton_6()
+    {
+        store.saveRecipe[5].SetActive(true);
+        SetfalseUI(5);
+    }
+    public void SetfalseUI(int num)
+    {
+        for (int i = 0; i < store.saveRecipe.Count; ++i)
+        {
+            if (i != num)
+            {
+                store.saveRecipe[i].SetActive(false);
+            }
+        }
 
+    }
     public void OpenPanel()
-    {
-        Debug.Log("열어");
+    {       
         recipePanel.SetActive(true);
     }
-
     public void ClosePanel()
-    {
-        Debug.Log("닫아");
-
-        recipePanel.SetActive(false); // 다시 닫아라
-
+    {       
+        recipePanel.SetActive(false);
     }
-
 }
--- Assets/Scripts/Store_Pro2.cs
+++ Assets/Scripts/Store_Pro2.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,41 +8,63 @@ public class Store_Pro2 : MonoBehaviour
 {
     public GameObject storeUI;
     public GameObject[] imagePrefabs; // 추가할 이미지의 프리팹
-   
+
     public Transform[] contents; // ScrollView의 Content 배열
 
     public GameObject selectedImagePrefab;
+    public GameObject selectedUIPrefab;
 
     private bool isImageAdded = false; // 이미지가 추가되었는지 여부를 나타내는 플래그
+    private bool isUIAdded = false;
     public Recipe_Pro recipePro;
+
+    public List<GameObject> saveRecipe;
 
     public void SetSelectedImagePrefab(GameObject selectedPrefab)
     {
         selectedImagePrefab = selectedPrefab;
         isImageAdded = false; // 새로운 이미지를 선택하면 플래그 초기화
     }
+    public void SetSelectedUI(GameObject selectedUI) // 선택한 UI 저장
+    {
+        selectedUIPrefab = selectedUI;
+        SaveRecipeImage(); // 선택한 UI 리스트에 추가
+        //isUIAdded = false; // 새로운 UI를 선택하면 플래그 초기화 // 추후 필요가능성 O
+    }
 
     public void AddImage()
     {
+        // 24.03.08 수정 필요 : 클릭한 이미지 이미 생성되어 있으면 카운트만 증가하기
+        //          :  만약 이미 선택한 이미지를 재선택 할 경우 이미 선택한 UI 는 선택되지 않도록 하기
         if (selectedImagePrefab != null && !isImageAdded)
         {
             for (int i = 0; i < contents.Length; i++)
             {
                 if (!ContainsImage(contents[i]))
                 {
+
                     // 클릭한 이미지 프리팹을 사용하여 이미지 생성
                     GameObject newImage = Instantiate(selectedImagePrefab, contents[i]);
 
                     // Content의 크기 조정
                     RectTransform contentRectTransform = contents[i].GetComponent<RectTransform>();
                     contentRectTransform.sizeDelta += new Vector2(0, newImage.GetComponent<RectTransform>().sizeDelta.y);
+                    isUIAdded = true;
 
-
-                    isImageAdded = true; // 이미지 추가 true
+                    //isImageAdded = true; // 이미지 추가 true
                     break;
                 }
             }
         }
+    }
+    public void SaveRecipeImage()
+    {
+        if (saveRecipe.Count < 6) // 5개까지만 추가
+        {
+            saveRecipe.Add(selectedUIPrefab);
+        }
+
+
     }
 
     public bool ContainsImage(Transform content)
@@ -49,10 +72,7 @@ public class Store_Pro2 : MonoBehaviour
         return content.childCount > 0; // content에 이미지프리팹 있는지 확인
     }
 
-    public void OnClickButton()
-    {
 
-    }
 
     public void OpenStoreUI()
     {
